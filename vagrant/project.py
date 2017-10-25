2e50bee3aa1dd7d2a57402e803622c3f3ebdf154
Adinsxx <c.mccurrie@gmail.com>
2017-10-25 13:36:00 -0500

    make it better

--- vagrant/project.py
+++ vagrant/project.py
@@ -456,7 +456,7 @@ def disconnect():
 		return redirect(url_for('showBreweries'))
 
 
-if __name__ == '__main__':
-	app.secret_key = 'eat_my_shorts'
-	app.debug = True
-	app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+app.secret_key = 'eat_my_shorts'
+app.debug = True
+# app.run(host='0.0.0.0', port=5000)
